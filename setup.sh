ab09acc6b932f6ea1a47f2d8f1d7c0b64fc1e931
zhangxiao <zhangx9411@gmail.com>
2022-06-23 21:11:33 +0800

    Init again

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 
-
 # 1. Setup segmentation code, based on MMSegmentation
 
 # 1.1 Install mmcv
@@ -7,23 +6,21 @@ pip install -U openmim
 mim install mmcv-full
 
 # 1.2 Install MMSegmentation
-pip install -v -e /workspace/mae_downtask/mae_seg/mmsegmentation
+pip install -v -e /MIMDownTasks/mim_seg/mmsegmentation
 
 
 # 2. Setup detection code, based on Detectron2
 
 # 2.1 Install Detectron2
-pip install -v -e /workspace/mae_downtask/mae_det/detectron2/
+pip install -v -e /MIMDownTasks/mim__det/detectron2
 # python -m pip install -e detectron2
-
-
 
 
 # 3 Setup soft link for ADE20K and coco dataset
 wget -p /dev/shm http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
-unzip /dev/shm/ADEChallengeData2016.zip /dev/shm
-ln -s /dev/shm/ADEChallengeData2016 /workspace/mae_downtask/mae_seg/mmsegmentation/data/ade/ADEChallengeData2016
+unzip /dev/shm/ADEChallengeData2016.zip -d /dev/shm
+ln -s /dev/shm/ADEChallengeData2016 /MIMDownTasks/mim_seg/mmsegmentation/data/ade/ADEChallengeData2016
 
 # 3.2 setup coco data under /data
-export DETECTRON2_DATASETS=/data
+# export DETECTRON2_DATASETS=/data
 
